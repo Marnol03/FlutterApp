eec61ace8bad950cb43a69c726994e77472fcb0a
inf4159 <inf4159@hs-worms.de>
2023-11-30 19:18:43 +0100

    Support page und plegund anderen Seite

--- Spendeconnect/lib/authentificate_screen.dart
+++ Spendeconnect/lib/authentificate_screen.dart
@@ -4,7 +4,6 @@ import 'package:myapp/constant.dart';
 import 'package:myapp/authentification.dart';
 import 'loading.dart';
 
-
 class AuthentificateScreen extends StatefulWidget {
   const AuthentificateScreen({super.key});
 
@@ -18,8 +17,8 @@ class _AuthentificateScreenState extends State<AuthentificateScreen> {
   String error = '';
   bool loading = false;
 
-  final usernameController= TextEditingController();
-  final emailController= TextEditingController();
+  final usernameController = TextEditingController();
+  final emailController = TextEditingController();
   final passwordController = TextEditingController();
   bool showSignIn = true;
 
@@ -31,113 +30,116 @@ class _AuthentificateScreenState extends State<AuthentificateScreen> {
     super.dispose();
   }
 
-  void toggleview(){
+  void toggleview() {
     setState(() {
       _formkey.currentState?.reset();
-      error ='';
-      usernameController.text='';
-      emailController.text='';
-      passwordController.text='';
+      error = '';
+      usernameController.text = '';
+      emailController.text = '';
+      passwordController.text = '';
       showSignIn = !showSignIn;
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    return loading ? Loading() :
-    Scaffold(
+    return loading
+        ? Loading()
+        : Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
         elevation: 0.0,
         title: Text(
-          showSignIn ? 'Sign in ': 'Register',
+          showSignIn ? 'Sign in ' : 'Register',
         ),
-        actions:<Widget> [
+        actions: <Widget>[
           TextButton.icon(
-            icon: const Icon(Icons.person,
+            icon: const Icon(
+              Icons.person,
               color: Colors.white,
             ),
-            label: Text(showSignIn ? 'Register in' : 'Sign in',
-                style: const TextStyle(color: Colors.white)),
+            label: Text(
+              showSignIn ? 'Register in' : 'Sign in',
+              style: const TextStyle(color: Colors.white),
+            ),
             onPressed: () => toggleview(),
           )
         ],
-      ),body: Container(
-      padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 30.0),
-      child: Form(
-        key: _formkey,
-        child: (
-            Column(
-              children: [
-                if (!showSignIn) ...[
-                  TextFormField(
-                    controller: usernameController,
-                    decoration: textInputdecoration.copyWith(hintText: "entrez votre username"),
-                    validator: (value) => value!.isEmpty ? "entrez votre username " : null,
-                  ),
-                ] else Container(),
-                SizedBox(height: 10.0),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 30.0),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              if (!showSignIn) ...[
                 TextFormField(
-                  controller: emailController,
-                  decoration: textInputdecoration.copyWith(hintText: "entrez votre email"),
-                  validator: (value) => value!.isEmpty ? "entrez votre email" : null,
+                  controller: usernameController,
+                  decoration: textInputdecoration.copyWith(hintText: "Enter your username"),
+                  validator: (value) => value!.isEmpty ? "Enter your username" : null,
                 ),
-                SizedBox(height: 10.0),
-                TextFormField(
-                  controller: passwordController,
-                  decoration: textInputdecoration.copyWith(hintText: "entrez votre mot de passe"),
-                  obscureText: true,
-                  validator: (value) =>
-                  value!.length < 6 ? "entrez un mot de passe valide" : null,
+              ] else
+                Container(),
+              SizedBox(height: 10.0),
+              TextFormField(
+                controller: emailController,
+                decoration: textInputdecoration.copyWith(hintText: "Enter your email"),
+                validator: (value) => value!.isEmpty ? "Enter your email" : null,
+              ),
+              SizedBox(height: 10.0),
+              TextFormField(
+                controller: passwordController,
+                decoration: textInputdecoration.copyWith(hintText: "Enter your password"),
+                obscureText: true,
+                validator: (value) =>
+                value!.length < 6 ? "Enter a valid password" : null,
+              ),
+              SizedBox(height: 10.0),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
                 ),
-                SizedBox(height: 10.0),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blueAccent,
-                  ),
-                  child: Text(
-                    showSignIn ? "sign In" : "Register",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onPressed: () async {
-                    if (_formkey.currentState!.validate()) {
-                      setState(() => loading = true);
-                      var password = passwordController.value.text;
-                      var email = emailController.value.text;
-                      var username = usernameController.value.text;
+                child: Text(
+                  showSignIn ? "Sign In" : "Register",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () async {
+                  if (_formkey.currentState!.validate()) {
+                    setState(() => loading = true);
+                    var password = passwordController.value.text;
+                    var email = emailController.value.text;
+                    var username = usernameController.value.text;
 
-                      dynamic result = showSignIn
-                          ? await _auth.signInWithEmailAndPassword(email, password)
-                          : await _auth.registerInWithEmailAndPassword(username,email, password);
+                    dynamic result = showSignIn
+                        ? await _auth.signInWithEmailAndPassword(email, password)
+                        : await _auth.registerInWithEmailAndPassword(username, email, password);
 
-                      CollectionReference postRef = FirebaseFirestore.instance.collection("users");
-                      postRef.add({
-                        "email": email,
-                        "username": username,
+                    CollectionReference postRef =
+                    FirebaseFirestore.instance.collection("users");
+                    postRef.add({
+                      "email": email,
+                      "username": username,
+                    });
+
+                    if (result == null) {
+                      setState(() {
+                        loading = false;
+                        error = 'Enter a valid email address';
                       });
-
-
-                      if (result == null) {
-                        setState(() {
-                          loading = false;
-                          error = 'entrez une adresse mail valide svp';
-                        });
-                      }
                     }
-                  },
-                ),
-                SizedBox(height: 10.0),
-                Text(
-                  error,
-                  style: TextStyle(color: Colors.red, fontSize: 15),
-                )
-              ],
-            )
-
+                  }
+                },
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                error,
+                style: TextStyle(color: Colors.red, fontSize: 15),
+              )
+            ],
+          ),
         ),
       ),
-    ),
-
     );
   }
 }
--- Spendeconnect/lib/help_page.dart
+++ Spendeconnect/lib/help_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:myapp/support_page.dart';
 
 import 'authentification.dart';
 
@@ -20,21 +21,23 @@ class _ProfilPageState extends State<ProfilPage> {
   @override
   void initState() {
     super.initState();
+    // Fetch user data when the widget is initialized
     fetchUserData();
   }
 
+  // Fetch user data from Firestore
   Future<void> fetchUserData() async {
     try {
-      // Récupérer l'utilisateur actuellement connecté
+      // Get the currently signed-in user
       User? user = _auth.getCurrentUser();
 
       if (user != null) {
-        // Récupérer les données de Firestore pour l'utilisateur actuel
+        // Retrieve Firestore data for the current user
         DocumentSnapshot userSnapshot =
         await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
 
         if (userSnapshot.exists) {
-          // Mettre à jour l'état avec les données récupérées
+          // Update state with the retrieved data
           Map<String, dynamic> userData = userSnapshot.data() as Map<String, dynamic>;
           setState(() {
             username = userData['username'];
@@ -43,12 +46,12 @@ class _ProfilPageState extends State<ProfilPage> {
         }
       }
     } catch (e) {
-      print("Erreur lors de la récupération des données : $e");
+      print("Error fetching data: $e");
     }
   }
 
-  Future<List<DocumentSnapshot>> getCollectionData(
-      String collectionName) async {
+  // Fetch data from a Firestore collection
+  Future<List<DocumentSnapshot>> getCollectionData(String collectionName) async {
     List<DocumentSnapshot> documentList = [];
 
     try {
@@ -57,7 +60,7 @@ class _ProfilPageState extends State<ProfilPage> {
 
       documentList = querySnapshot.docs;
     } catch (e) {
-      print("Erreur lors de la récupération des données : $e");
+      print("Error fetching data: $e");
     }
 
     return documentList;
@@ -76,28 +79,49 @@ class _ProfilPageState extends State<ProfilPage> {
               backgroundImage: AssetImage('assets/images/profil.png'),
             ),
             const SizedBox(height: 25),
+            // Display user profile information
             itemProfile('Username', username, CupertinoIcons.person),
             const SizedBox(height: 12),
             itemProfile('Email', email, CupertinoIcons.mail),
             const SizedBox(height: 25),
 
             const SizedBox(height: 25),
+            // Logout button
             ElevatedButton.icon(
               style: const ButtonStyle(
                 backgroundColor: MaterialStatePropertyAll(Colors.green),
               ),
               onPressed: () async {
+                // Perform sign out action
                 await _auth.signOut();
               },
               icon: const Icon(Icons.person, color: Colors.white),
-              label: const Text('logout'),
+              label: const Text('Logout'),
             ),
           ],
         ),
       ),
+
+      // Floating action button for support
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
+        onPressed: () {
+          // Action to perform when the floating action button is clicked
+          Navigator.push(
+            context,
+            CupertinoPageRoute(
+              builder: (context) =>
+                  SupportPage(username: username, email: email),
+            ),
+          );
+        },
+        tooltip: 'Support',
+        child: Icon(Icons.support_agent),
+      ),
     );
   }
 
+  // Widget to display profile information
   Widget itemProfile(String title, String subtitle, IconData iconData) {
     return Container(
       decoration: BoxDecoration(

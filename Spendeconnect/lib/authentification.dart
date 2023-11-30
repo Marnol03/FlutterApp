87d5ef2debf419682a9d83799cedd47f8a3ffe8c
inf4159 <inf4159@hs-worms.de>
2023-11-30 14:15:00 +0100

    DATA DER USER darstellen

--- Spendeconnect/lib/authentification.dart
+++ Spendeconnect/lib/authentification.dart
@@ -1,51 +1,85 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:myapp/user.dart';
 
-class AuthentificationService{
+class AuthentificationService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  AppUser? _userFromFirebaseUser (User? user){
-    return user !=null ? AppUser( uid: user.uid) : null;
+  // Convert Firebase User to custom AppUser
+  AppUser? _userFromFirebaseUser(User? user) {
+    return user != null ? AppUser(uid: user.uid) : null;
   }
+
+  // Stream to listen for authentication state changes
   Stream<AppUser?> get user {
     return _auth.authStateChanges().map((user) => _userFromFirebaseUser(user));
   }
 
-  Future signInWithEmailAndPassword(String email,String password) async{
-
-    try{
-      UserCredential result =
-      await _auth.signInWithEmailAndPassword(email: email, password: password);
+  // Function to sign in with email and password
+  Future<AppUser?> signInWithEmailAndPassword(String email, String password) async {
+    try {
+      // Sign in with email and password
+      UserCredential result = await _auth.signInWithEmailAndPassword(email: email, password: password);
       User? user = result.user;
-      return _userFromFirebaseUser(user!) ;
-    }catch(exception){
-      print (exception.toString());
+
+      // Retrieve user data from Firestore
+      DocumentSnapshot userSnapshot = await FirebaseFirestore.instance.collection('users').doc(user?.uid).get();
+
+      if (userSnapshot.exists) {
+        // User data successfully retrieved from Firestore
+        AppUser appUser = AppUser.fromFirestore(userSnapshot);
+        return appUser;
+      } else {
+        // User has no data in Firestore
+        print("No Firestore data found for the user with ID: ${user?.uid}");
+        return null;
+      }
+    } catch (exception) {
+      // An error occurred during authentication or retrieving Firestore data
+      print("Error during login: $exception");
       return null;
     }
   }
 
-  Future registerInWithEmailAndPassword(String username,String email,String password) async{
-
-    try{
-      UserCredential result = 
-      await _auth.createUserWithEmailAndPassword(email: email, password: password);
+  // Function to register a new user with email, username, and password
+  Future registerInWithEmailAndPassword(String username, String email, String password) async {
+    try {
+      // Create a new user with email and password
+      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
       User? user = result.user;
 
-      // todo store new user firestore
-      return _userFromFirebaseUser(user!) ;
-    }catch(exception){
-      print (exception.toString());
+      // Add user data to Firestore
+      await FirebaseFirestore.instance.collection('users').doc(user?.uid).set({
+        'username': username,
+        'email': email,
+      });
+
+      // Retrieve user data from Firestore
+      DocumentSnapshot userSnapshot = await FirebaseFirestore.instance.collection('users').doc(user?.uid).get();
+      AppUser appUser = AppUser.fromFirestore(userSnapshot);
+
+      // Return the AppUser object
+      return appUser;
+    } catch (exception) {
+      // Handle registration errors
+      print(exception.toString());
       return null;
     }
   }
 
-  Future signOut()async{
-    try{
+  // Function to sign out
+  Future signOut() async {
+    try {
       return await _auth.signOut();
-    }catch(exception){
-      print (exception.toString());
+    } catch (exception) {
+      // Handle sign-out errors
+      print(exception.toString());
       return null;
     }
   }
 
+  // Function to get the current authenticated user
+  User? getCurrentUser() {
+    return _auth.currentUser;
+  }
 }
--- Spendeconnect/lib/user.dart
+++ Spendeconnect/lib/user.dart
@@ -1,4 +1,18 @@
-class AppUser{
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AppUser {
   final String? uid;
-  AppUser({this.uid});
+  final String? username;
+  final String? email;
+
+  AppUser({this.uid, this.username, this.email});
+
+  factory AppUser.fromFirestore(DocumentSnapshot document) {
+    Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+    return AppUser(
+      uid: document.id,
+      username: data['username'] as String?,
+      email: data['email'] as String?,
+    );
+  }
 }
